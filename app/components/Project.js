58de7e7189bde28849afc11f07608cfdfbe8b31b
Jawad Ahmad <ja5916367@gmail.com>
2024-12-31 12:51:29 +0500

    updatedPl

--- app/components/Project.js
+++ app/components/Project.js
@@ -34,19 +34,27 @@ const Project = () => {
     }
   }, [currentIndex]);
   return (
-    <section className='xl:h-[120vh] w-full md:h-[50vh] flex flex-col justify-evenly '>
-      <div className="flex justify-between xl:h-[10vh] w-full items-center md:h-[5vh] max-sm:h-[5vh]">
+    <section className='lg:h-[125vh] w-full md:h-[60vh] flex flex-col justify-between'>
+      <div className="flex justify-between lg:h-[10vh] w-full items-center md:h-[5vh] max-sm:h-[5vh]">
         <h1 className='uppercase text-[1.8vw] max-sm:text-[15px] md:text-[30px]'>Selected Case Studies</h1>
         <div className="flex items-center gap-[5vw] md:gap-[100px]">
           <span onClick={() => { handlePrev() }} className='p-[1vw] rounded-xl bg-[#237FC6] cursor-pointer text-white text-[1.8vw] md:text-[30px] max-sm:text-[12px]'><FaChevronLeft /></span>
           <span onClick={() => { handleNext() }} className='p-[1vw] rounded-xl bg-[#237FC6] cursor-pointer text-white text-[1.8vw] md:text-[30px] max-sm:text-[12px]'><FaChevronRight /></span>
         </div>
       </div>
-      <div className="relative xl:h-[100vh] w-full md:h-[30vh] overflow-hidden max-sm:h-[50vh]">
+      <div className="relative lg:h-[110vh] w-full md:h-[60vh] overflow-hidden max-sm:h-[50vh] sm:h-[50vh] lg:bg-green-800">
         {data.map((item, i) => (
-          <div key={i} ref={(el) => (slideRefs.current[i] = el)} className="xl:h-[100vh] md:h-[25vh] w-full absolute flex gap-[5vw]">
-            <div className="h-full w-[50%]">
-              <img className='xl:h-full w-full rounded-3xl object-cover object-top md:h-[20vh] max-sm:h-[30vh]' src="https://i.pinimg.com/736x/34/49/4e/34494e6f99d3636cc8bbb7a1f2bcda52.jpg"/>
+          <div key={i} ref={(el) => (slideRefs.current[i] = el)} className="h-[110vh] w-full flex gap-[3vw]">
+            <div className="lg:h-[110vh] w-[50%] max-sm:h-[31vh] sm:h-[36vh] md:h-[36vh] flex flex-wrap justify-between items-start gap-[1.61vw]">
+            <div className="overflow-hidden lg:h-[71.6%] max-sm:h-[20vh] sm:h-[25vh] md:h-[25vh] w-full rounded-2xl">
+                    <img className='h-full w-full object-cover object-top' src="https://i.pinimg.com/736x/fa/25/8a/fa258abdf02a626d9d6599a465e8b7c5.jpg" alt="" />
+                </div>
+                <div className="overflow-hidden lg:h-[25%] max-sm:h-[10vh] sm:h-[10vh] md:h-[10vh] w-[45%] rounded-2xl">
+                    <img className='h-full w-full object-cover object-top' src="https://i.pinimg.com/736x/fa/25/8a/fa258abdf02a626d9d6599a465e8b7c5.jpg" alt="" />
+                </div>
+                <div className="overflow-hidden lg:h-[25%] max-sm:h-[10vh] sm:h-[10vh] md:h-[10vh] w-[45%] rounded-2xl">
+                    <img className='h-full w-full object-cover object-top' src="https://i.pinimg.com/736x/fa/25/8a/fa258abdf02a626d9d6599a465e8b7c5.jpg" alt="" />
+                </div>
             </div>
             <div className="h-full w-[50%] max-sm:h-[50vh] max-sm:w-[50%] xl:pt-[5vw]">
               <h1 className='text-[3vw] max-sm:text-[20px]'>{item.head1}</h1>
